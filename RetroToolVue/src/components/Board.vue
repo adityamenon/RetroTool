e0d3e46988327b6a0e1c650a2dd47369b380a55d
Aditya M P <hello@adityamenon.co>
2017-05-06 14:32:45 +0530

    Editables done

--- RetroToolVue/src/components/Board.vue
+++ RetroToolVue/src/components/Board.vue
@@ -3,29 +3,95 @@
     <table>
       <thead>
       <th></th>
-      <th v-for="column in columns"> {{ column }} </th>
+      <th v-for="column in columns">
+        <span contenteditable="true" v-on:blur="changeColumnLabel(column.id, $event)">{{ column.label }}</span>
+        <br>
+        <button v-on:click="removeColumn(column.id)">-</button>
+      </th>
+      <th><button v-on:click="addColumn">+</button></th>
       </thead>
       <tbody>
       <tr v-for="row in rows">
-        <td>{{ row }}</td>
+        <td>
+          <button v-on:click="removeRow(row.id)">-</button>&nbsp;
+          <span contenteditable="true" v-on:blur="changeRowLabel(row.id, $event)">{{ row.label }}</span>
+        </td>
         <td v-for="column in columns">
         </td>
       </tr>
       </tbody>
     </table>
+    <button v-on:click="addRow">+</button>
 
     <textarea cols="30" rows="10" v-model="actionItems" placeholder="Action Items"></textarea>
   </section>
 </template>
 
+
 <script>
+  import _ from 'lodash'
+
   export default {
     name: 'board',
     data () {
       return {
-        columns: ['Continue', 'Start Doing', 'Stop Doing'],
-        rows: ['Engineering Practices', 'Performance', 'Business Requirements Analysis'],
+        columns: [
+          {
+            label: 'Continue Doing',
+            id: Math.random()
+          },
+          {
+            label: 'Start Doing',
+            id: Math.random()
+          },
+          {
+            label: 'Stop Doing',
+            id: Math.random()
+          }
+        ],
+        rows: [
+          {
+            label: 'Engineering Practices',
+            id: Math.random()
+          },
+          {
+            label: 'Performance',
+            id: Math.random()
+          },
+          {
+            label: 'Requirements Analysis',
+            id: Math.random()
+          }
+        ],
         actionItems: 'Action Items:'
+      }
+    },
+    methods: {
+      removeColumn: function (id) {
+        this.columns = _.filter(this.columns, column => column.id !== id)
+      },
+      removeRow: function (id) {
+        this.rows = _.filter(this.rows, row => row.id !== id)
+      },
+      addColumn () {
+        this.columns.push({label: 'New Column', id: Math.random()})
+      },
+      addRow () {
+        this.rows.push({label: 'New Row', id: Math.random()})
+      },
+      changeColumnLabel (id, ev) {
+        this.columns.splice(
+          _.findIndex(this.columns, column => column.id === id),
+          1,
+          {id: id, label: ev.target.innerText}
+        )
+      },
+      changeRowLabel (id, ev) {
+        this.rows.splice(
+          _.findIndex(this.rows, row => row.id === id),
+          1,
+          {id: id, label: ev.target.innerText}
+        )
       }
     }
   }
@@ -36,5 +102,8 @@
   th, td {
     border: 1px solid black;
     padding: 20px;
+  }
+  th {
+    text-align: center;
   }
 </style>
