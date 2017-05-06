226e59d62b20d7cf5db29606f0451204c0c75357
Aditya M P <hello@adityamenon.co>
2017-05-06 12:44:55 +0530

    First dyanmic piece

---
+++ RetroToolVue/src/components/Board.vue
@@ -0,0 +1,36 @@
+<template>
+  <table>
+    <thead>
+      <th></th>
+      <th v-for="column in columns"> {{ column }} </th>
+    </thead>
+    <tbody>
+      <tr v-for="row in rows">
+        <td>{{ row }}</td>
+        <td v-for="column in columns">
+
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  export default {
+    name: 'board',
+    data () {
+      return {
+        columns: ['Continue', 'Start Doing', 'Stop Doing'],
+        rows: ['Engineering Practices', 'Performance', 'Business Requirements Analysis']
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  th, td {
+    border: 1px solid black;
+    padding: 20px;
+  }
+</style>
--- RetroToolVue/src/router/index.js
+++ RetroToolVue/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Board from '@/components/Board'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/board',
+      name: 'Board',
+      component: Board
     }
   ]
 })
