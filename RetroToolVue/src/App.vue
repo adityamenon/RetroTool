10a2cabad2ee8342db278540309617698f182409
Pankaj Mishra <pankaj.mishra@srijan.net>
2017-05-06 14:40:12 +0530

    ui init

--- RetroToolVue/src/App.vue
+++ RetroToolVue/src/App.vue
@@ -11,10 +11,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+    html, body{
+        font-family: 'Ubuntu', sans-serif;
+    }
+    #app {
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      color: #2c3e50;
+    }
 </style>
--- RetroToolVue/src/components/Board.vue
+++ RetroToolVue/src/components/Board.vue
@@ -1,20 +1,24 @@
 <template>
-  <section>
-    <table>
-      <thead>
-      <th></th>
-      <th v-for="column in columns"> {{ column }} </th>
-      </thead>
-      <tbody>
-      <tr v-for="row in rows">
-        <td>{{ row }}</td>
-        <td v-for="column in columns">
-        </td>
-      </tr>
-      </tbody>
-    </table>
-
-    <textarea cols="30" rows="10" v-model="actionItems" placeholder="Action Items"></textarea>
+  <section class="retro-board-container flex">
+    <div class="retro-board-wrapper flex">
+      <table class="retro-board flex">
+        <thead class="retro-board-header-wrapper flex">
+          <tr class="retro-board-header flex">
+            <th></th>
+            <th v-for="column in columns"> {{ column }} </th>
+          </tr>
+        </thead>
+        <tbody class="retro-board-body-wrapper">
+          <tr class="flex" v-for="row in rows">
+            <td>{{ row }}</td>
+            <td v-for="column in columns"></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="action-notes-wrapper">
+      <textarea cols="30" rows="10" v-model="actionItems" placeholder="Action Items"></textarea>
+    </div>
   </section>
 </template>
 
@@ -24,7 +28,7 @@
     data () {
       return {
         columns: ['Continue', 'Start Doing', 'Stop Doing'],
-        rows: ['Engineering Practices', 'Performance', 'Business Requirements Analysis'],
+        rows: ['Engineering Practices', 'Performance, Debugging & Devops', 'Business Requirement Analysis', 'Knowledge Sharing & Contribution', 'Scrum Team Functions'],
         actionItems: 'Action Items:'
       }
     }
@@ -33,7 +37,42 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  th, td {
+  .flex{
+    display: flex;
+  }
+  .block{
+    display: block;
+  }
+  .i-block{
+    display: inline-block;
+  }
+  .retro-board-container {
+    width: 100%;
+    display: flex;
+    align-items: flex-start;
+  }
+  .retro-board-wrapper {
+    width: 80%;
+    padding: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: stretch;
+  }
+  .retro-board{
+    width: 100%;
+    display: flex;
+  }
+  .retro-board-header-wrapper{
+  }
+  .action-notes-wrapper{
+    width: 20%;
+    padding: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: stretch;
+  }
+  th,td {
+    display: block;
     border: 1px solid black;
     padding: 20px;
   }
